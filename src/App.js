41f52ddb0abd04dc8f20cc297a45266780f62f40
MacBook Air <mac@MacBooks-MacBook-Air.local>
2022-12-08 02:34:58 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+ const usersArray = [
+  {name: 'User1'},
+  {name: 'User2'},
+  {name: 'User3'},
+  {name: 'User4'},
+  {name: 'User5'},
+  {name: 'User6'},
+  {name: 'User7'},
+  {name: 'User8'},
+  {name: 'User9'},
+  {name: 'User10'},
+ ];
+ const GenerateUsers = () =>{
+  return usersArray.map((user)=>{
+    return user.name
+  })
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function App() {
+  
+  
+  const [users,setUsers] = useState(GenerateUsers());
+  
+  const clickHandler = () =>{
+    const element = users[getRandomInt(users.length)];
+    setUsers(users.filter(function(e) { return e !== element }));
+  };
+
+  useEffect(()=>{  
+    document.title = users.length + ' users left'; 
+    },[users]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='header'>users left:</div>
+     <div className='userslist'>
+        {users.map((user) => (
+            <p>{user}</p>
+        ))}
+     </div>
+     <button onClick={clickHandler}>click</button>
     </div>
   );
 }
